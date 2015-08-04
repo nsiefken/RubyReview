888df8946cead4915519d557966f84f7a32fc18e
Nick Siefken <nicks720@gmail.com>
2015-08-04 10:35:06 -0500

    finish fizzbuzz

--- ruby/fizz-buzz/my_solution.rb
+++ ruby/fizz-buzz/my_solution.rb
@@ -1,34 +1,35 @@
-# U3.W8-9:
 
-# I worked on this challenge [by myself, with: ].
-# This challenge took me [#] hours.
-
-
-# Pseudocode
-
-
-
-# Initial Solution
 
 def super_fizzbuzz(array)
-
-
+	fizzbuzzed_array = []
+	array.each do |number|
+		if number % 5 == 0 && number % 3 == 0
+			fizzbuzzed_array << "FizzBuzz"
+		elsif number % 3 == 0
+			fizzbuzzed_array << "Fizz"
+		elsif number % 5 == 0
+			fizzbuzzed_array << "Buzz"
+		else
+			fizzbuzzed_array << number
+		end
+	end	
+	return fizzbuzzed_array
 end
 
+# def fizzbuzz
+# 	i = 1
+# 	until i > 100
+# 		if i % 5 == 0 && i % 3 == 0
+# 			puts "FizzBuzz"
+# 		elsif i % 3 == 0
+# 			puts "Fizz"
+# 		elsif i % 5 == 0
+# 			puts "Buzz"
+# 		else
+# 			puts i
+# 		end
+# 		i += 1
+# 	end	
+# end
 
-
-# Refactored Solution
-
-
-
-
-
-
-# DRIVER TESTS GO BELOW THIS LINE
-
-
-
-
-
-
-# Reflection
+# fizzbuzz
